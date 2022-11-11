8d71792398ca4d6fbe73b7076f54b8de4fea841e
rosanno <rosannomacabuhay@gmail.com>
2022-11-12 07:45:48 +0800

    fix redirect error

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -19,35 +19,6 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
-const TAX_RATE = 0.07;
-
-function ccyFormat(num) {
-  return `${num.toFixed(2)}`;
-}
-
-function priceRow(qty, unit) {
-  return qty * unit;
-}
-
-function createRow(desc, qty, unit) {
-  const price = priceRow(qty, unit);
-  return { desc, qty, unit, price };
-}
-
-function subtotal(items) {
-  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
-}
-
-const rows = [
-  createRow('Paperclips (Box)', 100, 1.15),
-  createRow('Paper (Case)', 10, 45.99),
-  createRow('Waste Basket', 2, 17.99),
-];
-
-const invoiceSubtotal = subtotal(rows);
-const invoiceTaxes = TAX_RATE * invoiceSubtotal;
-const invoiceTotal = invoiceTaxes + invoiceSubtotal;
-
 const Order = () => {
   const navigate = useNavigate();
   const auth = useSelector((state) => state.auth);
@@ -55,6 +26,7 @@ const Order = () => {
   const [removeOrder] = useRemoveOrderMutation();
 
   React.useEffect(() => {
+    console.log(auth?.token);
     if (!auth?.token) {
       navigate('/');
     }
